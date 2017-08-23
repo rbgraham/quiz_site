16f0eb5cd3a37e6f0eb686a2b915fbcdb2f8d57a
Robert Graham <rgraham@whitetailsoftware.com>
2017-08-22 20:48:29 -0700

    Add heroku config.

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :quiz_site, QuizSiteWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "fast-earth-690027.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
