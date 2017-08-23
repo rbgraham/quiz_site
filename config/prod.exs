edf6dd29666f59dd448e7494cb9930242de7b77a
Robert Graham <rgraham@whitetailsoftware.com>
2017-08-22 20:44:30 -0700

    Add heroku config.

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :quiz_site, QuizSiteWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "fast-earth-690027.herokuapp.com", port: 443],
+force_ssl: [rewrite_on: [:x_forwarded_proto]],,
   cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,6 +61,8 @@ config :logger, level: :info
 #     config :quiz_site, QuizSiteWeb.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :quiz_site, QuizSite.Repo,
+  adapter: Ecto.Adapter.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
--- lib/quiz_site_web/channels/user_socket.ex
+++ lib/quiz_site_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule QuizSiteWeb.UserSocket do
   # channel "room:*", QuizSiteWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
