db4038234c6ad78717932fc4897bde38ecfd2ab6
Robert Graham <rgraham@whitetailsoftware.com>
2017-08-22 20:54:02 -0700

    Fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -62,7 +62,7 @@ config :logger, level: :info
 #
 
 config :quiz_site, QuizSite.Repo,
-  adapter: Ecto.Adapter.Postgres,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
