db175d4b43eb52eb925862f8195b10e103aaeb49
Robert Graham <rgraham@whitetailsoftware.com>
2017-09-25 19:45:37 -0700

    Fix config typo.

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :quiz_site, QuizSiteWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "fast-earth-690027.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "fast-earth-69002.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
