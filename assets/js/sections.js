676c2d6a61aeacbca63b6000e9f46ef783ab9aae
Robert Graham <rgraham@whitetailsoftware.com>
2017-10-24 19:13:42 -0700

    Reorganize JS.

--- assets/js/sections.js
+++ assets/js/sections.js
@@ -1,18 +1,189 @@
-import axios from "axios"
 import React from "react"
+import { Cards } from "./simple_cards.js"
+
+import {
+  ShareButtons,
+  generateShareIcon
+} from 'react-share';
+
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  RedditShareButton,
+  EmailShareButton,
+} = ShareButtons;
+
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const LinkedinIcon = generateShareIcon('linkedin');
+const EmailIcon = generateShareIcon('email');
 
 class Section extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+      super(props);
+      this.state = { 
+        section: props.section,
+        click: props.click,
+        choices: props.choices,
+        score: props.score,
+        emailClick: props.emailClick,
+        skip: props.skip,
+        emailKeyPress: props.emailKeyPress,
+        drip_id: props.drip_id,
+        result_id: props.result_id,
+        back: props.back,
+        email_set: props.email_set,
+        result_display: props.result_display
+      }
+    }
 
-  render() {
-    return (
-      <div>
-        <h2></h2>
-      </div>
-    );
-  }
+    shouldDisplay() {
+      if (this.state.section.conditions.length == 0) {
+        /* 
+         * Handle whether or not this is a results oriented shared page.
+         * The result page context may choose to show or hide certain sections
+         * so the content is relevant to the viewer.
+         */
+        if (this.state.result_display == false && this.state.section.result_display == false) {
+          return true;
+        } else if (this.state.result_display == true && this.state.section.result_display == true) {
+          return true;
+        } else if ((this.state.result_display == true && this.state.section.result_display == false) ||
+                  (this.state.result_display == false && this.state.section.result_display == true)) {
+          return false;
+        }
+      } else if (this.scoreSection()) { 
+        var cond = this.state.section.conditions[0];
+        return this.checkScoreCondition(cond);
+      } else {
+        var conditions = [];
+        this.state.section.conditions.forEach((c) => {
+          conditions.push( this.state.choices.find((e) => {
+            return e == c.condition;
+          }) );
+        });
 
+        if (conditions.filter((c) => { return c != undefined; }).length < conditions.length) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+
+    scoreSection () {
+      if (this.state.section.conditions.length == 1 && this.scoreCondition(this.state.section.conditions[0])) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    scoreCondition(cond) {
+      if (cond.equal_to || cond.greater_than || cond.less_than) {
+        return true;
+      }
+      return false;
+    }
+
+    checkScoreCondition(cond) {
+      if (cond.equal_to) {
+        return cond.equal_to == this.state.score;
+      }
+
+      var condition = true;
+      if (cond.greater_than) {
+        condition = (cond.greater_than < this.state.score);
+      }
+
+      if (cond.less_than) {
+        condition = condition && (cond.less_than > this.state.score);
+      }
+      return condition;
+    }
+
+    emailForm(props) {
+      return Cards.emailForm(props);
+    }
+
+    render() {
+      let cta = null;
+      let share = null;
+      if (this.state.section.share) {
+        /* TODO This isn't quite right. I need a data model for the tweet text.
+        */
+        var tweet_url = window.location.protocol + "//" + window.location.host + "/results/" + this.state.result_id;
+        var tweet_text = this.state.section.share_text;
+        share = (
+          <div className="col-md-offset-4 col-md-4 col-xs-4 col-xs-offset-3">
+            <FacebookShareButton url={ tweet_url } quote={ tweet_text } className="col-md-1 col-xs-1">
+              <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+            <TwitterShareButton url={ tweet_url } title={ tweet_text } className="col-md-1 col-xs-1">
+              <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+            <LinkedinShareButton url={ tweet_url } title={ tweet_text } className="col-md-1 col-xs-1">
+              <LinkedinIcon size={32} round={true} />
+            </LinkedinShareButton>
+            <EmailShareButton url={ tweet_url } subject={ tweet_text } className="col-md-1 col-xs-1">
+              <EmailIcon size={32} round={true} />
+            </EmailShareButton>
+          </div>
+        );
+      }
+
+      /*
+       * A little bit (OK a lot) hacky to check the CTA string for making
+       * behavior, but I blame this being the last TODO before release.
+       */
+      if (this.state.section.cta && this.state.section.cta.includes("Go back")) {
+        if (!this.state.email_set) {
+          cta = <button className="btn btn-warning btn-sm h5 btn--email-set" onClick={ this.state.back }>{this.state.section.cta}</button>;
+        }
+      } else if (this.state.section.cta && this.state.section.cta.includes("Take the quiz!")) {
+        cta = <div><a className="btn btn-warning btn-large h4 btn--email-set" href="/" >{this.state.section.cta}</a></div>;
+      } else if (this.state.section.cta) {
+        cta = <button className="btn btn-warning btn-large h4" onClick={ this.state.click }>{ this.state.section.cta }</button>;
+      }
+
+      let img = null;
+      if (this.state.section.image_path) {
+        const src = Cards.image_path(this.state.section.image_path);
+        img = (
+          <img src={src} width="100%" className="img-fluid center-block" />
+        );
+      }
+
+      var rendered = (
+          <div className="text-center col-md-8 col-md-offset-2">
+            <div className="img-container">
+              { img }
+            </div>
+            <h1 >
+            { this.state.section.title }
+            </h1>
+            <p className="h5">{ this.state.section.content }</p>
+            <br/>
+            { cta }
+            { share }
+          </div>
+      );
+
+      if (this.shouldDisplay() && !this.state.section.email_form) {
+        return rendered;
+      } else if (this.shouldDisplay() && this.state.section.email_form) {
+        return this.emailForm(this.state);
+      } else {
+        return Cards.blank();
+      }
+    }
 }
+
+export { Section };
--- assets/js/simple_cards.js
+++ assets/js/simple_cards.js
@@ -1,66 +1,131 @@
 import React from "react"
 
 var Cards = { 
-  title: function (props) {
-    return (
-        <div>
-          <h1>{props.card.title}</h1>
-        </div>
-    );
-  },
-
   blank: function (props) {
     return (
       <div></div>
     );
   },
-  
-  section: function (props) {
-    return (
-      <div>
-        <h1>
-        { props.title }
-        </h1>
+
+  emailForm: function (props) {
+    var skip = null;
+    if (props.section.skip_button) {
+      skip = (
         <div>
-          <p>
-            { props.content }
-          </p>
+          <button className="btn btn-default btn-sm h5 btn--skip"
+            type="submit" name="skip"
+            onClick={ props.skip }>
+            Skip &gt;
+          </button>
         </div>
-        <button>{ props.cta }</button>
+      );
+    }
+    var form = (
+    <div className="text-center form-group col-md-6 col-md-offset-3 needs-validation">
+      <h3 data-drip-attribute="headline">{ props.section.title }</h3>
+      <div data-drip-attribute="description">{ props.section.content }</div>
+        <div>
+            <input className="form-control"
+              type="email" 
+              id="drip-email" 
+              name="fields[email]" 
+              placeholder="you@domain.com"
+              onKeyPress={ props.emailKeyPress }
+              required/>
+            <div className="invalid-feedback" id="email-error" style={{ display: 'none' }}>
+              Please provide a valid email address.
+            </div>
+        </div>
+      <div>
+        <button className="btn btn-warning btn-large h4"
+          type="submit" name="submit"
+          data-drip-attribute="sign-up-button"
+          data-choice="full-report"
+          onClick={ props.emailClick }>
+          { props.section.cta }
+        </button>
       </div>
+      { skip }
+    </div>
     );
+    return form;
+  },
+    
+  choice: function (props) {
+    let img = null;
+    var display = ( 
+      <button className="btn btn-default choice big-btn" onClick={props.click} 
+        value={props.choice.id} data-choice={props.choice.choice}
+        data-choice-score={props.choice.score}>
+        { props.choice.choice }
+      </button>
+    );
+
+    if (props.choice.image_path) {
+      const src = Cards.image_path(props.choice.image_path);
+      let microcopy = null;
+      if (props.choice.microcopy) {
+        microcopy = props.choice.choice;
+      }
+      img = (
+          <button className="btn btn-default choice" onClick={props.click} 
+            value={props.choice.id} data-choice={props.choice.choice}
+            data-choice-score={props.choice.score}>
+            <img src={src} height="100px;" className="center-block"/>
+            { microcopy }
+          </button>
+      );
+      display = img;
+    }
+
+    return display;
   },
 
   question: function (props) {
+    let subtext = null;
+    if (props.question.subtext) {
+      subtext = (
+        <span className="small">
+          { props.question.subtext }
+        </span>
+      );  
+    }
+    var choices = [];
+    props.question.choices.sort((choice, choice_b) => {
+      return choice.id - choice_b.id;
+    }).forEach((choice) => {
+      choices.push(<Cards.choice key={choice.choice} choice={choice} click={ props.click }/>);
+    });
     return (
-      <div>
-        <h1>
-        { props.question }
-        <span class="small">{ props.subtext }</span>
-        </h1>
-      </div>
-    );
+        <div className="text-center">
+          <h1>
+          { props.question.question }
+          </h1>
+          <div>
+          { subtext }
+          </div>
+          <div className="choices text-center">
+            { choices }
+          </div>
+        </div>
+      );
   },
+
+  layout: class Layout extends React.Component {
+    constructor(props){
+      super(props);
+    }
+    componentWillUpdate(nextProps) {
+      document.title = nextProps.title;
+    }
+    render(){
+      return null;
+    }
+  },
+
+  image_path: function (path) {
+    return "/images/" + path;
+  }
 }
 
-class TitleCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = props.card;
-  }
-
-  renderSection (section) {
-    return Cards.section(section);
-  }
-
-  render() {
-    return (
-      <div>
-        { this.renderSection(this.state.sections[0]) }
-      </div>
-    );
-  }
-
-}
-
-export { Cards, TitleCard };
+export { Cards };
